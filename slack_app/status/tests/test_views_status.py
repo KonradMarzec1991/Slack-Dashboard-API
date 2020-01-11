cfc9aeb3b387afb7d5a1d89de0f92595e9cd8248
KonradMarzec1991 <konrimarzec@gmail.com>
2020-01-11 21:13:02 +0100

    Fix pytest.ini and tests

--- slack_app/status/tests/test_views_status.py
+++ slack_app/status/tests/test_views_status.py
@@ -13,35 +13,20 @@ def status_values():
 def status_response(key=None):
     client = Client()
     if key is None:
-        response = client.get('http://127.0.0.1:8000/status/')
+        response = client.get('/status/')
     else:
-        response = client.get(f'http://127.0.0.1:8000/status/{key}')
+        response = client.get(f'/status/{key}/')
     return response
 
 
-# def test_status_view_response(status_values):
-#     response = status_response()
-#     assert response.status_code == 200
-#     assert response.data == status_values
-#
-#
-# def test_status_view_response_key():
-#     response = status_response('commit')
-#     assert response.status_code == 200
-#     response = status_response('_wrong')
-#     assert response.status_code == 404
+def test_status_view_response(status_values):
+    response = status_response()
+    assert response.status_code == 200
+    assert response.data == status_values
 
 
-from unittest import TestCase
-
-
-class StatusViewTestCase(TestCase):
-
-    def setUp(self):
-        self.values = {'commit': '2874#952', 'version': '1.0.0'}
-        self.client = Client()
-
-    def test_status_response_view(self):
-        response = self.client.get('http://127.0.0.1:8000/status/')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, self.values)
+def test_status_view_response_key():
+    response = status_response('_wrong')
+    assert response.status_code == 404
+    response = status_response('commit')
+    assert response.status_code == 200
