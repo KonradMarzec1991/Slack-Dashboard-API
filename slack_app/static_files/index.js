2e429fdc084eaf836ab8cd326a55b0ebae151e8f
KonradMarzec1991 <konrimarzec@gmail.com>
2020-02-15 20:16:35 +0100

    Send initial request to backend

--- slack_app/static_files/index.js
+++ slack_app/static_files/index.js
@@ -1,2 +1,31 @@
 const start = document.getElementById('start');
 start.innerText = 'start';
+
+
+const getJSON = () => {
+    let p = new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', '/namespaces');
+        xhr.responseType = 'json';
+
+        xhr.onload = () => {
+            resolve(xhr.response);
+        };
+
+        xhr.onerror = () => {
+            reject(new Error('Something went wrong'));
+        };
+
+        xhr.send()
+    });
+    return p;
+};
+
+
+const renderHTML = arr => {
+    arr.forEach(el => {
+        console.log(el.name);
+    })
+};
+
+getJSON().then(res => renderHTML(res));
