4df8f9b5d3f6441fc538eb357cde84854e9706a8
KonradMarzec1991 <konrimarzec@gmail.com>
2020-02-19 19:08:45 +0100

    Load workspaces from backend

--- slack_app/static_files/index.js
+++ slack_app/static_files/index.js
@@ -1,7 +1,9 @@
+
+
 const getJSON = () => {
     let p = new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest();
-        xhr.open('GET', '/namespaces');
+        xhr.open('GET', '/namespaces/1/tickets/');
         xhr.responseType = 'json';
 
         xhr.onload = () => {
@@ -17,11 +19,33 @@ const getJSON = () => {
     return p;
 };
 
+// Create workspace / channel hierarchy
 
-const renderHTML = arr => {
-    arr.forEach(el => {
-        console.log(el.name);
-    })
-};
+getJSON().then(res => {
+    let workspaces = res.workspace;
+    let htmlWorkspaces = document.createElement('ul');
+    htmlWorkspaces.classList.add('sidebar-workspace');
 
-getJSON().then(res => renderHTML(res));
+    for (let key of Object.keys(workspaces)) {
+        let workspace = document.createElement('li');
+        workspace.innerHTML = `${key}`;
+        htmlWorkspaces.appendChild(workspace);
+
+        if (typeof workspaces[key] == 'object') {
+            let channels = document.createElement('ul');
+            channels.classList.add('sidebar-channels');
+            htmlWorkspaces.appendChild(channels);
+
+            for (let channel of workspaces[key]) {
+                console.log(channel);
+                let channelItem = document.createElement('li');
+                channelItem.innerHTML = `${channel}`;
+                channels.appendChild(channelItem);
+            }
+            htmlWorkspaces.appendChild(channels);
+        }
+        const sidebar = document.querySelector('.sidebar-content');
+        sidebar.appendChild(htmlWorkspaces);
+    }
+
+});
