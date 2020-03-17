1acb1d3462bcd88d8cdb883539fc7a50e395c05b
KonradMarzec1991 <konrimarzec@gmail.com>
2020-03-17 19:40:13 +0100

    Create utils.py file

--- slack_app/slack/views.py
+++ slack_app/slack/views.py
@@ -1,5 +1,6 @@
 from celery import shared_task
 
+from .utils import create_ticket
 from django.views.decorators.csrf import csrf_exempt
 from django.http.response import HttpResponse
 import requests
@@ -177,37 +178,4 @@ def proceed_payload(request):
             return HttpResponse(status=200)
 
 
-@shared_task
-def create_ticket(data_dict, reporter, channel_id, team_id, response_url):
-    a = Actions(channel_id)
 
-    title = data_dict['submission']['title']
-    description = data_dict['submission']['description']
-    status = data_dict['submission']['status']
-    severity = data_dict['submission']['severity']
-
-    workspace = a.get_workspace(team_id)
-    channel = a.get_channel(channel_id)
-
-    ticket_data = {
-        'namespace': Namespace.objects.get(id=1),  # temporary solution
-        'title': title,
-        'description': description,
-        'status': status,
-        'severity': severity,
-        'reporter': reporter,
-        'data': {
-            'channel': channel,
-            'workspace': workspace
-        }
-    }
-
-    Ticket.objects.create(**ticket_data)
-
-    data = json.dumps({
-        'token': a.token,
-        'text': 'Ticket has been created'
-    })
-
-    requests.post(response_url, data=data)
-    return {'status': 200}
